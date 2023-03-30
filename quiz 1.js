4d5f6ae00500728acc3dec74858f2fd0907608c5
Harrvvin Raj <you@example.comharrvvinr@gmail.com>
2023-03-30 16:01:52 +0800

    rename

---
+++ quiz 1.js
@@ -0,0 +1,38 @@
+//let message = "benr"
+//console.log(message, typeof message) 
+
+message = 123
+//console.log (message, typeof message)
+
+
+
+let str = "1.258"                        //INTEGER NUMBER
+let num = 1
+
+//let total = parseFloat(str) + num
+//console.log(total, typeof total)
+
+let data =["BENR", 3523, true]            // ARRAY
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+
+
+
+let user = {                            //OBJECT 
+    name: "HARRVVIN",
+    age: 25,
+    facukty: "FKEKK"
+}
+//console.log(user.name)
+//console.log(user.age)
+
+
+
+let messages =[
+    {name: "user1",date:"1/4/2023",msg:"good video",like:1000},   
+    {name: "user2",date :"2/4/2023",msg:"very good video",like:3000}, 
+]     
+console.log(messages[1].msg)
